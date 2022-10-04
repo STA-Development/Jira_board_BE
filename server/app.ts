c32f7dd912751261b8af39164664a915bd50ed74
Amir <mikelsonj000@gmail.com>
2022-10-04 17:29:28 +0400

    Creating swagger

--- server/app.ts
+++ server/app.ts
@@ -1,20 +1,75 @@
-import express from 'express';
-import sequelize from './database';
-const app = express();
 
-app.get("/", (req : Express.Request, res : Express.Response) => {
- // res.send("welcome to website");
-  
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+//import sequelize from './database';
+const app = express();
+const swaggerOptions = {
+swaggerDefinition:{
+  info : {
+    title : "JiraBoardLibrary",
+    version : "1.0.0",
+    description : "JiraBoard API"
+  }
+  },
+apis:['app.ts'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+/**
+ * @swagger
+ * /JiraBoard:
+ *   get:
+ *     tags:
+ *     name: Todo
+ *     summary: Write your works and descripe it
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             title:
+ *               type: string
+ *             description:
+ *               type: string
+ *         required:
+ *           - title
+ *           - description
+ *     responses:
+ *       '200':
+ *         description: successfully registered
+ *       '403':
+ *         description: expects is not completed
+ */
+
+
+app.get("/JiraBoard", (req , res ) => {
+ res.send([{
+id : 1,
+title : 'Sleep',
+description : 'sleep soon'
+ }])
 });
-app.post('/post',(req , res , next) => {
+app.post('/JiraBoard',(req , res , next) => {
   res.send("yeah!!!!");
   next();
-})
+});
+
+/*
 sequelize.authenticate().then(() => {
   console.log('Connection has been established successfully.');
 }).catch((error) => {
   console.error('Unable to connect to the database: ', error);
 });
+*/
+
+
 
 const PORT = 5000;
 
